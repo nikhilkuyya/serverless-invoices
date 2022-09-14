34a7385f2f5f91ef24fbb97625874d29df9bbd82
nikhilkuyya <nikhilkuyya@gmail.com>
2022-09-14 10:05:02 +0530

    feat: update condition

--- src/filters/dineroCurrency.filter.js
+++ src/filters/dineroCurrency.filter.js
@@ -1,11 +1,11 @@
-import Dinero from 'dinero.js';
+import Dinero from "dinero.js";
 
 export function formatCurrencyWithDinero(value, factor = 10 ** 2) {
-  if (Number.isNaN(value)) {
-    return '';
+  if (Number.isNaN(Number(value))) {
+    return "";
   }
   const amount = Number.parseFloat(value).toFixed(2) * factor;
-  const dineroObject = Dinero({ amount: amount, currency: 'INR' });
+  const dineroObject = Dinero({ amount: amount, currency: "INR" });
   const dineroAmount = dineroObject.toRoundedUnit(2);
-  return Intl.NumberFormat('en-IN', { currency: 'INR', style: 'currency' }).format(dineroAmount);
+  return Intl.NumberFormat("en-IN", { currency: "INR", style: "currency" }).format(dineroAmount);
 }
